ac1d5e310ff423051c1882bd797ac14215b14cf4
root <root@node1.ybqjzobwvcyezilxpmxdjmmbbe.gx.internal.cloudapp.net>
2019-03-07 11:14:57 +0000

    service start added

--- script.sh
+++ script.sh
@@ -2,10 +2,10 @@ echo "Starting postaction on VM" >> /tmp/vm.log
 
 sudo yum -y install git
 sudo yum -y install docker
-
+sudo service docker start
 sudo mkdir /app
 sudo cd /app
 sudo git clone https://github.com/EqualExperts/node-js-getting-started.git nodejs
 sudo cp /tmp/Dockerfile /app/
-docker build -t akhilesh0/nodejs-starter .
-docker run -d -P --name nodejs akhilesh0/nodejs-starter
+sudo docker build -t akhilesh0/nodejs-starter .
+sudo docker run -d -P --name nodejs akhilesh0/nodejs-starter
