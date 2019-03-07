72917eaa905b45f70fed1bbd128944d29e916d85
root <root@node1.ybqjzobwvcyezilxpmxdjmmbbe.gx.internal.cloudapp.net>
2019-03-07 12:41:08 +0000

    updated script

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 echo "Starting postaction on VM" >> /tmp/vm.log
 
-echo "demopass@123" | sudo yum -y install git
-echo "demopass@123" | sudo yum -y install docker
-echo "demopass@123" | sudo service docker start
-echo "demopass@123" | sudo mkdir /app
-echo "demopass@123" | sudo cd /app
-echo "demopass@123" | sudo git clone https://github.com/EqualExperts/node-js-getting-started.git nodejs
-echo "demopass@123" | sudo cp /tmp/Dockerfile /app/
-echo "demopass@123" | sudo docker build -t akhilesh0/nodejs-starter .
-echo "demopass@123" | sudo docker run -d -P --name nodejs akhilesh0/nodejs-starter
+echo "demopass@123" | sudo -S yum -y install git
+echo "demopass@123" | sudo -S yum -y install docker
+echo "demopass@123" | sudo -S service docker start
+echo "demopass@123" | sudo -S mkdir /app
+echo "demopass@123" | sudo -S cd /app
+echo "demopass@123" | sudo -S git clone https://github.com/EqualExperts/node-js-getting-started.git nodejs
+echo "demopass@123" | sudo -S cp /tmp/Dockerfile /app/
+echo "demopass@123" | sudo -S docker build -t akhilesh0/nodejs-starter .
+echo "demopass@123" | sudo -S docker run -d -P --name nodejs akhilesh0/nodejs-starter
