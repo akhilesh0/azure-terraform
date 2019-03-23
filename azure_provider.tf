87ce9b6925c22f75fd8a119cd26e8ae383d4fad9
Akhilesh Kumar <akhi1990khalari@gmail.com>
2019-03-23 13:05:49 +0530

    Update azure_provider.tf

--- azure_provider.tf
+++ azure_provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" { 
-  subscription_id = "ab3f4084-36b1-42ad-a908-77f988eb46d5"
-  client_id = "416a8a40-ec8b-4c4d-81d7-0a65d8aaa6db"
-  client_secret = "C/DlOJKpF1wBBGN2ftF1La5MOaI2q+FnnObyLhZQ0oU="
-  tenant_id = "50310dc7-b3a8-43e9-9ea7-4965d47523d3"
+  subscription_id = ""
+  client_id = ""
+  client_secret = ""
+  tenant_id = ""
 }
